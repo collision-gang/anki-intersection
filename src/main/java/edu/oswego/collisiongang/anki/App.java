7596caa7b2f7c0f02c55517091d40712282191a2
Stephen Iskander <siskande@oswego.edu>
2019-04-10 11:17:06 -0400

    maven project

---
+++ src/main/java/edu/oswego/collisiongang/anki/App.java
@@ -0,0 +1,13 @@
+package edu.oswego.collisiongang.anki;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
