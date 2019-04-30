2e77c4e4166fb5f378f82549af5a9ec7aba507f8
Stephen Iskander <siskande@oswego.edu>
2019-04-30 13:39:14 -0400

    it stop

--- src/main/java/edu/oswego/collisiongang/anki/App.java
+++ src/main/java/edu/oswego/collisiongang/anki/App.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class App 
 {
     static final boolean DEBUG = true;
+    static Vehicle ego;
     public static void main( String[] args ) throws IOException
     {
         if (DEBUG) System.out.printf("Launching connector...\n");
@@ -20,9 +21,26 @@ public class App
         if (DEBUG) System.out.printf("Getting vehicle list...\n");
         List<Vehicle> vehicles = connector.findVehicles();
 
-        Vehicle ego = vehicles.stream().findFirst().get();
+        if (DEBUG) System.out.printf("Selecting first vehicle...\n");
+        ego = vehicles.stream().findFirst().get();
+        if (DEBUG) System.out.printf("Connecting...\n");
         ego.connect();
+        LocalizationPositionUpdateListener listener = new LocalizationPositionUpdateListener();
+        ego.addMessageListener(LocalizationPositionUpdateMessage.class, listener);
         ego.sendMessage(new SdkModeMessage());
+
         ego.sendMessage(new SetSpeedMessage(1000, 0));
+
+    }
+
+    private static class LocalizationPositionUpdateListener implements MessageListener<LocalizationPositionUpdateMessage> {
+        private int lastId = -1;
+
+        @Override
+        public void messageReceived(LocalizationPositionUpdateMessage m) {
+            if (m.getRoadPieceId() == 10) {
+                ego.sendMessage(new SetSpeedMessage(0, 1000));
+            }
+        }
     }
 }
