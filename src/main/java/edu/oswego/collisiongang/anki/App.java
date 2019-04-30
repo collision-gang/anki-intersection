d446dd57a369e210507d17ae06d8ba42f98cc853
Stephen Iskander <siskande@oswego.edu>
2019-04-30 13:08:11 -0400

    it move

--- src/main/java/edu/oswego/collisiongang/anki/App.java
+++ src/main/java/edu/oswego/collisiongang/anki/App.java
@@ -1,13 +1,28 @@
 package edu.oswego.collisiongang.anki;
 
+import de.adesso.anki.*;
+import de.adesso.anki.messages.*;
+
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    static final boolean DEBUG = true;
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        if (DEBUG) System.out.printf("Launching connector...\n");
+        AnkiConnector connector = new AnkiConnector("localhost", 5000);
+        if (DEBUG) System.out.printf("Getting vehicle list...\n");
+        List<Vehicle> vehicles = connector.findVehicles();
+
+        Vehicle ego = vehicles.stream().findFirst().get();
+        ego.connect();
+        ego.sendMessage(new SdkModeMessage());
+        ego.sendMessage(new SetSpeedMessage(1000, 0));
     }
 }
