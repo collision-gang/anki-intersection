0e198a6387266dfdc9dc227ccdcf2f92f1b54887
Stephen Iskander <siskande@oswego.edu>
2019-05-02 20:06:37 -0400

    they stop and go

--- src/main/java/edu/oswego/collisiongang/anki/App.java
+++ src/main/java/edu/oswego/collisiongang/anki/App.java
@@ -5,41 +5,26 @@ import de.adesso.anki.messages.*;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ForkJoinPool;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     static final boolean DEBUG = true;
-    static Vehicle ego;
-    public static void main( String[] args ) throws IOException
-    {
-        if (DEBUG) System.out.printf("Launching connector...\n");
-        AnkiConnector connector = new AnkiConnector("localhost", 5000);
-        if (DEBUG) System.out.printf("Getting vehicle list...\n");
-        List<Vehicle> vehicles = connector.findVehicles();
+    static final String HOST = "localhost";
+    static final int PORT = 5000;
 
-        if (DEBUG) System.out.printf("Selecting first vehicle...\n");
-        ego = vehicles.stream().findFirst().get();
-        if (DEBUG) System.out.printf("Connecting...\n");
-        ego.connect();
-        LocalizationPositionUpdateListener listener = new LocalizationPositionUpdateListener();
-        ego.addMessageListener(LocalizationPositionUpdateMessage.class, listener);
-        ego.sendMessage(new SdkModeMessage());
+    public static void main( String[] args ) throws IOException {
+        AnkiConnector connector = new AnkiConnector(HOST, PORT);
 
-        ego.sendMessage(new SetSpeedMessage(1000, 0));
-
-    }
-
-    private static class LocalizationPositionUpdateListener implements MessageListener<LocalizationPositionUpdateMessage> {
-        private int lastId = -1;
-
-        @Override
-        public void messageReceived(LocalizationPositionUpdateMessage m) {
-            if (m.getRoadPieceId() == 10) {
-                ego.sendMessage(new SetSpeedMessage(0, 1000));
+        List<Vehicle> vehicleList = connector.findVehicles();
+        if (!vehicleList.isEmpty()) {
+            System.out.println(vehicleList.size());
+            int controllerPort = 4269;
+            for (Vehicle v : vehicleList) {
+                ForkJoinPool.commonPool().execute(new VehicleController(v, controllerPort++));
             }
         }
     }
---
+++ src/main/java/edu/oswego/collisiongang/anki/VehicleController.java
@@ -0,0 +1,62 @@
+package edu.oswego.collisiongang.anki;
+
+import de.adesso.anki.MessageListener;
+import de.adesso.anki.Vehicle;
+import de.adesso.anki.messages.LocalizationIntersectionUpdateMessage;
+import de.adesso.anki.messages.LocalizationPositionUpdateMessage;
+import de.adesso.anki.messages.SdkModeMessage;
+import de.adesso.anki.messages.SetSpeedMessage;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class VehicleController implements Runnable {
+    private Vehicle ego;
+    private ServerSocket peerServer;
+    private AtomicBoolean arrivedAtIntersection = new AtomicBoolean(false);
+    private AtomicBoolean navigatingIntersection = new AtomicBoolean(false);
+
+    public VehicleController(Vehicle ego, int port) throws IOException {
+        this.ego = ego;
+        peerServer = new ServerSocket(port);
+    }
+
+    @Override
+    public void run() {
+        ego.connect();
+        ego.sendMessage(new SdkModeMessage());
+        ego.addMessageListener(LocalizationPositionUpdateMessage.class, new LocalizationPositionUpdateListener());
+        ego.addMessageListener(LocalizationIntersectionUpdateMessage.class, new LocalizationIntersectionUpdateListener());
+        ego.sendMessage(new SetSpeedMessage(300, 1000));
+
+        for (;;) {
+            if (arrivedAtIntersection.get()) {
+                arrivedAtIntersection.compareAndSet(true, false);
+                navigatingIntersection.compareAndSet(false, true);
+                long arrivalTime = System.currentTimeMillis();
+                ego.sendMessage(new SetSpeedMessage(0, 1000));
+                while(System.currentTimeMillis() - arrivalTime < 3000);
+                ego.sendMessage(new SetSpeedMessage(300, 1000));
+            }
+        }
+    }
+
+    class LocalizationPositionUpdateListener implements MessageListener<LocalizationPositionUpdateMessage> {
+
+        @Override
+        public void messageReceived(LocalizationPositionUpdateMessage m) {
+            if (!navigatingIntersection.get() && m.getRoadPieceId() == 10) {
+             arrivedAtIntersection.compareAndSet(false, true);
+            }
+        }
+    }
+
+    class LocalizationIntersectionUpdateListener implements MessageListener<LocalizationIntersectionUpdateMessage> {
+
+        @Override
+        public void messageReceived(LocalizationIntersectionUpdateMessage m) {
+            if (m.isExiting()) navigatingIntersection.compareAndSet(true, false);
+        }
+    }
+}
